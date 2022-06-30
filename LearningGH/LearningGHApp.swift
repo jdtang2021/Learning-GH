815a2b98d4dfbaadca4f929ace2ae930c43c9b0a
Jay Tang <jdtang33@gmail.com>
2022-06-30 12:28:40 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jay Tang on 30/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
